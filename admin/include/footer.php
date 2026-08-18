409c088a164fc2a77f3f2a309fb2e48d84ed4388
Louis Karanja <karanjalouis55@gmail.com>
2026-08-18 20:21:46 +0300

    Initial commit - HOTSPORT Gym Management System

---
+++ admin/include/footer.php
@@ -0,0 +1,16 @@
+<footer class="">
+		<div class="container">
+			
+			<div class="footer-bottom">
+				<div class="row">
+					
+					<div class="col-md-8 text-md-right">
+						<div class="copyright"> 
+Hotsport Gym <?php echo date('Y') ?></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+		
